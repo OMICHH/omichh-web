81f77c12b6efee8bca5c703b5359315869e2607e
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-12-09 15:26:25 -0700

    Karel tutorials models

--- karel/models.py
+++ karel/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from blog.models import Tag
 
-# Create your models here.
+
+class Problems(models.Model):
+    problem_name = models.CharField(max_length=150)
+    url = models.URLField(max_length=255)
+
+    def __str__(self):
+        return str(self.problem_name)
+
+
+class Karel(models.Model):
+
+    title = models.CharField(max_length=150)
+    tag = models.ManyToManyField(Tag)
+    problem_set = models.ManyToManyField(Problems)
+    order = models.IntegerField()
+    video_url = models.URLField(max_length=255)
+
+    def __str__(self):
+        return str(self.title)
+
