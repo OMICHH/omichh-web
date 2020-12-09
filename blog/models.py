86109c093a0592f747d499289b89e3e66ce88eb6
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-12-09 16:00:13 -0700

    blog models

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Tag(models.Model):
@@ -7,3 +8,26 @@ class Tag(models.Model):
 
     def __str__(self):
         return str(self.tag_name)
+
+
+class Post(models.Model):
+
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag)
+    post_title = models.CharField(max_length=255)
+    publish_date = models.DateTimeField()
+    content = models.TextField()
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.post_title)
+
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    comments = models.TextField()
+
+    def __str__(self):
+        return str(self.id)
