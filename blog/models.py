88cafda54b90b585d637f30c6a4827bdde6cd8e5
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-12-09 11:34:21 -0700

    tags for videos and post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+
+    tag_name = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return str(self.tag_name)
