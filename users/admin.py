d546553cf8fb73d71399813273f25889c5ec3b88
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-12-09 11:09:59 -0700

    Users admin config

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,37 @@
+#Django
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from django.contrib.auth.models import User
+from users.models import Coach, Student, School, Category
+
+
+@admin.register(Coach)
+class CoachAdmin(admin.ModelAdmin):
+    list_display = ('coach_user', 'coach_email','omegaup_user')
+    list_display_links = ('coach_user','coach_email','omegaup_user')
+    search_fields = ('coach_user','coach_email','omegaup_user')
+    list_filter =  ('coach_user__is_active',)
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+
+    list_display = ('student_user', 'coach', 'school', 'omegaup_user')
+    list_display_links = ('student_user',)
+    search_fields = ('student_user', 'omegaup_user')
+    list_filter =  ('student_user__is_active',)
+
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('school','principal_name','principal_email')
+    list_display_links = ('school','principal_name','principal_email')
+    search_fields = ('school','principal_name','principal_email')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name','school_level')
+    list_display_links = ('category_name','school_level')
+    search_fields = ('category_name','school_level')
--- users/models.py
+++ users/models.py
@@ -6,11 +6,24 @@ from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
 
 
+class Category(models.Model):
+
+    category_name = models.CharField(max_length=10, blank=False, null=False)
+    school_level = models.CharField(max_length=50, blank=False, null=False)
+
+    def __str__(self):
+        return str(self.school_level)
+
+    def Meta(self):
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+
+
 class School(models.Model):
 
     school = models.CharField(max_length=250, blank=True, null=True)
     principal_name = models.CharField(max_length=100, blank=True, null=True)
-    principal_email = models.CharField(max_length=100, blank=True, null=True)
+    principal_email = models.EmailField(max_length=254)
 
     def __str__(self):
         return str(self.school)
@@ -20,24 +33,31 @@ class Coach(models.Model):
 
     coach_user = models.OneToOneField(User, on_delete=models.CASCADE)
     school = models.ManyToManyField(School)
+    coach_email = models.EmailField(max_length=254)
     birthdate = models.DateField(blank=True, null=True)
-    phone = PhoneNumberField()
+    phone = PhoneNumberField(null=False, blank=False, unique=True)
     omegaup_user = models.CharField(max_length=150)
+    category = models.ManyToManyField(Category)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.coach_user.username)
+
+    def Meta(self):
+        verbose_name = _('Coach')
+        verbose_name_plural = _('Coaches')
 
 
 class Student(models.Model):
 
     student_user = models.OneToOneField(User, on_delete=models.CASCADE)
     coach = models.ForeignKey(Coach, on_delete=models.SET_NULL, null=True)
-    school = models.ManyToManyField(School)
+    school = models.ForeignKey(School,on_delete=models.SET_NULL, null=True)
     birthdate = models.DateField(blank=True, null=True)
     name_of_tutor = models.CharField(max_length=150, blank=True, null=False)
-    phone_of_tutor = PhoneNumberField()
-    tutor_email = models.CharField(max_length=100, blank=True, null=True)
+    phone_of_tutor = PhoneNumberField(null=False, blank=False, unique=True)
+    tutor_email = models.EmailField(max_length=254)
     omegaup_user = models.CharField(max_length=150)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.student_user.username)
