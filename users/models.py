ac0d38156d7f6a748c5b723fe779f2ea6b72834c
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-12-08 23:32:19 -0700

    Users models

--- users/models.py
+++ users/models.py
@@ -2,13 +2,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+#phone number field
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class School(models.Model):
+
+    school = models.CharField(max_length=250, blank=True, null=True)
+    principal_name = models.CharField(max_length=100, blank=True, null=True)
+    principal_email = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.school)
+
 
 class Coach(models.Model):
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    school = models.ManyToManyField(School, verbose_name=_("Schools"))
+    coach_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    school = models.ManyToManyField(School)
     birthdate = models.DateField(blank=True, null=True)
-    phone = models.PhoneNumberField()
+    phone = PhoneNumberField()
     omegaup_user = models.CharField(max_length=150)
 
     def __str__(self):
@@ -17,21 +30,14 @@ class Coach(models.Model):
 
 class Student(models.Model):
 
-    user = models.OneToOneField(User, verbose_name=_("Students"), on_delete=models.CASCADE)
-    coach = models.OneToOneField(Coach, verbose_name=_("Coaches"), on_delete=models.SET_NULL)
-    school = models.ManyToManyField(School, verbose_name=_("Schools"))
+    student_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    coach = models.ForeignKey(Coach, on_delete=models.SET_NULL, null=True)
+    school = models.ManyToManyField(School)
     birthdate = models.DateField(blank=True, null=True)
-    name_of_tutor = models.CharField(max_length=150, ${blank=True, null=False})
-    phone_of_tutor = models.PhoneNumberField()
-    tutor_email = name = models.CharField(max_length=100, ${blank=True, null=True})
+    name_of_tutor = models.CharField(max_length=150, blank=True, null=False)
+    phone_of_tutor = PhoneNumberField()
+    tutor_email = models.CharField(max_length=100, blank=True, null=True)
     omegaup_user = models.CharField(max_length=150)
 
-
-class School(models.Model):
-
-    name = models.CharField(max_length=250, ${blank=True, null=True})
-    principal_name = name = models.CharField(max_length=100, ${blank=True, null=True})
-    principal_email = name = models.CharField(max_length=100, ${blank=True, null=True})
-
     def __str__(self):
-        return str(self.name)
+        return str(self.user.username)
