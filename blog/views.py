2a10ca2a101ad92c7cdd7bd1b420a9bc810fc203
SaulTapia <19080045@cobachih.edu.mx>
2021-04-06 15:15:23 -0600

    Add blog view with dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def blog_view(request):
+
+    # Dummy data
+    posts = [
+        {
+            'title' : 'Soy el primer titulo',
+            'description' : 'Soy la descripción del primero',
+            'date' : '18/02/2021',
+            'url' : '#'
+        },
+        {
+            'title' : 'Soy el segundo titulo',
+            'description' : 'Soy la descripción del segundo',
+            'date' : '26/02/2021',
+            'url' : '#'
+        },
+        {
+            'title' : '"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."',
+            'description' : 'ta bn',
+            'date' : '02/03/2021',
+            'url' : '#'
+        },
+        {
+            'title' : 'mini titulo',
+            'description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sollicitudin ultricies dolor, scelerisque facilisis ante finibus nec.',
+            'date' : '??/??/????',
+            'url' : '#'
+        },
+        {
+            'title' : 'Soy un filler',
+            'description' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            'date' : '??/??/????',
+            'url' : '#'
+        },
+        {
+            'title' : 'Soy un filler',
+            'description' : 'Soy la descripción del filler',
+            'date' : '??/??/????',
+            'url' : '#'
+        },
+        {
+            'title' : 'Soy un filler',
+            'description' : 'Soy la descripción del filler',
+            'date' : '??/??/????',
+            'url' : '#'
+        }
+    ]
+
+    context = {
+        'posts' : posts
+    }
+
+    return render(request, 'blog/blog.html', context)
